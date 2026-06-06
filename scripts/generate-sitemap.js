2bacaae8d122539a1ce85a99b529cb619803ab02
Juan Pablo Ortega <jportegame@gmail.com>
2026-06-05 19:15:21 -0500

    update: sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -12,7 +12,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const siteUrl = 'https://dlula.com';
+const siteUrl = 'https://dlulacoliving.com';
 
 // Manual configuration based on current siteConfig
 const staticPages = [
