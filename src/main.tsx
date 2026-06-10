7dc8a1dcec3640253186c6ae37b3d4a5466d3ad2
Juan Pablo Ortega <jportegame@gmail.com>
2026-06-10 12:07:18 -0500

    add: hash router

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 // @ts-ignore
 import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
         <SpeedInsights />
-      </BrowserRouter>
+      </HashRouter>
     </HelmetProvider>
   </StrictMode>,
 )
