5a917aa51204176d66d5b54d00cdfabb28003e66
Juan Pablo Ortega <jportegame@gmail.com>
2026-07-23 09:42:42 -0500

    add scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import { useRoutes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useLocation, useRoutes } from 'react-router-dom'
 import { routes } from './router/routes'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'auto',
+    })
+  }, [pathname])
+
+  return null
+}
 
 function App() {
   const element = useRoutes(routes)
-  return element
+
+  return (
+    <>
+      <ScrollToTop />
+      {element}
+    </>
+  )
 }
 
 export default App
